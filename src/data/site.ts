92daa8360821df0641db65910a7a908d669af44e
basso1515-max <basso1515@gmail.com>
2026-08-07 13:21:16 -0300

    Add site configuration and demo photos

---
+++ src/data/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  couple: "GUILHERME & SABRINA",
+  date: "31/10/2027",
+  location: "Paróquia Nossa Senhora do Perpétuo Socorro",
+  subtitle: "Nosso dia, contado por todos que estiverem conosco.",
+};
+
+export const demoPhotos = [
+  { src: "/images/demo-1.svg", alt: "Espaço reservado para uma foto do casal" },
+  { src: "/images/demo-2.svg", alt: "Espaço reservado para uma foto do casal" },
+  { src: "/images/demo-3.svg", alt: "Espaço reservado para uma foto do casal" },
+  { src: "/images/demo-4.svg", alt: "Espaço reservado para uma foto do casal" },
+  { src: "/images/demo-5.svg", alt: "Espaço reservado para uma foto do casal" },
+  { src: "/images/demo-6.svg", alt: "Espaço reservado para uma foto do casal" },
+];
